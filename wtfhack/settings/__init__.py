081e2a4e2e98e385cae1671c69638db825e10e8a
Peter Collins <pmcollins2@wisc.edu>
2013-02-02 01:26:59 -0600

    Add code that actually works

--- wtfhack/settings/__init__.py
+++ wtfhack/settings/__init__.py
@@ -4,6 +4,4 @@ from .base import *
 try:
     from .local import *
 except ImportError, exc:
-    #exc.args = tuple(
-    #    ['%s (did you rename settings/local-dist.py?)' % exc.args[0]])
-    #raise exc
+    print '%s (did you rename settings/local-dist.py?)' % exc.args[0]
