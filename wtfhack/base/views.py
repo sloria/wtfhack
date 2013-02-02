8897919a079667fb96ef7340370a76661d3d716a
Peter Collins <pmcollins2@wisc.edu>
2013-02-02 15:02:09 -0600

    Got github repos and view

--- wtfhack/base/views.py
+++ wtfhack/base/views.py
@@ -7,8 +7,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import simplejson
 
 from wtfhack.base.models import *
+from social_auth.models import UserSocialAuth
+from github import Github
+
 import random
 
+from pprint import pprint
 
 def home(request):
     """ View for the home page """
@@ -39,12 +43,12 @@ def get_repo(request, language):
                 )
 				
 def submit(request):
-    """ View for the home page """
-    if Repo.objects.exists():
-        languages = Language.all()
-        random_language = random.choice(languages)
-        return HttpResponseRedirect(reverse('get_repo', args=(random_language,)))
-    else:
-        return render(request, 'base/home.html')
+    """ View for the submit page """
+    user = UserSocialAuth.objects.filter(provider='github').get(id=request.user.id)
+    print user.tokens[u'access_token']
+    github = Github(user.tokens[u'access_token'])
+    repos = [repo for repo in github.get_user().get_repos()]
+    print repos
+    return render(request, 'base/submit.html', {'repos': repos})
 
 
