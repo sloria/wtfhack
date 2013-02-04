e8c3e64048be876a97a618235720cdfecde87000
Steven Loria <sloria1@gmail.com>
2013-02-03 23:25:45 -0600

    Fix submission

--- wtfhack/base/urls.py
+++ wtfhack/base/urls.py
@@ -6,10 +6,10 @@ from wtfhack.base.views import *
 
 urlpatterns = patterns('wtfhack.base.views',
     url(r'^$', 'home', name='home'),
-	url(r'^submit/$', 'submit', name='submit'),
+	url(r'^submit/choose/$', 'submit', name='submit'),
     # ex: language/scala/
     url(r'^(?P<language>[\w\-\+]+)/$', get_repo, name='get_repo'),
     # ex: add_repo/
-    url(r'^add_repo/$', add_repo, name='add_repo'),
+    url(r'^submit/add_repo/$', add_repo, name='add_repo'),
 
 )
